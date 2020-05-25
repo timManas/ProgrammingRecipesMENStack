a992f81fc83aac04f83da73d989bd811a750fb0b
TimManas <timothy.romero.manas@gmail.com>
2020-05-25 17:09:33 -0400

    Finished CRUD operations and modified Create

--- CreateAPIRequest/public/browser.js
+++ CreateAPIRequest/public/browser.js
@@ -4,11 +4,46 @@
 
 // Initializers
 
+// Display Logic
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+      <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+  </li>`
+}
+
+// Initial Page load Render
+let ourHTML = items.map(function(item){     // Note: Map will Actually return an Array
+    return itemTemplate(item)
+}).join("")
+document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML)
+
 // Event Logic
+// CRUD - Create Functionality
+let createField = document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    axios.post("/create-item", { text: createField.value }).then(function (response) {
+        // Create HTML for new item
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value = ""
+        createField.focus()
+    }).catch(function () {
+        console.log("Please try again later")
+    })          
+})
+
+
+// Event Logic of Clicking
 document.addEventListener("click", function(event) {
 
     // CRUD - Replace Functionality
     if (event.target.classList.contains("edit-me")) {
+        console.log("Edit Button was clicked")
         let userInput = prompt("you clicked Edit button", event.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
 
         if (userInput) {
@@ -24,6 +59,7 @@ document.addEventListener("click", function(event) {
 
     // CRUD - Delete Functionality
     if (event.target.classList.contains("delete-me")) {
+        console.log("Delete Button was clicked")
         if (confirm("Do you want to Delete Permanately ?")) {        // Create a confirm pop up
             axios.post("/delete-item", { id: event.target.getAttribute("data-id") }).then(function () {
                 event.target.parentElement.parentElement.remove()
--- CreateAPIRequest/server.js
+++ CreateAPIRequest/server.js
@@ -21,63 +21,65 @@ mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: tr
 // API Request
 app.get('/', function(req, res) {
     db.collection('items').find().toArray(function(err, items) {
-      res.send(`<!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-    <div class="container">
-    <h1 class="display-4 text-center py-1">To-Do App!</h1>
-    
-    <div class="jumbotron p-3 shadow-sm">
-    <form action="/create-item" method="POST">
-    <div class="d-flex align-items-center">
-    <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-    <button class="btn btn-primary">Add New Item</button>
-    </div>
-    </form>
-    </div>
-    
-    
-    <ul class="list-group pb-5">
-      ${items.map(function(item) {
-        return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">${item.text}</span>
-        <div>
-        <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-        <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-        </li>`
-      }).join('')}
-    </ul>
-    </div>
-    
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>  
-    <script src="/browser.js"></script>
+        // When sending we use "BACK TICKS `` and NOT quotations"
+        res.send(`<!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Simple To-Do App</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        </head>
+        <body>
+          <div class="container">
+            <h1 class="display-4 text-center py-1">To-Do App !!! </h1>
+            
+            <div class="jumbotron p-3 shadow-sm">
+              <form id="create-form" action="/create-item" method="POST">
+                <div class="d-flex align-items-center">
+                  <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                  <button class="btn btn-primary">Add New Item</button>
+                </div>
+              </form>
+            </div>
+            
+            <ul id="item-list" class="list-group pb-5">
+            
+            </ul>
+            
+          </div>
+        
 
-    </body>
-    </html>`)
+        <script>
+        // Client Side Rendering
+        let items = ${JSON.stringify(items)} 
+        </script>
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>  
+        <script src="/browser.js"></script>  
+        </body>
+        </html>`)        
     })
   })
 
 // CRUD - 'C' Create Post using insertOne()
 app.post('/create-item', function(req, res) {
-    db.collection("items").insertOne({text: req.body.item}, function(err, info) {
-      console.log("Mongo Database should have this  message: " + req.body.item)
-      res.redirect('/')
+    console.log("Sending to DB: New Item")
+    db.collection("items").insertOne({text: req.body.text}, function(err, info) {
+        console.log("Create Request -  Mongo Database should have this  message: " + req.body.item)
+        res.json(info.ops[0])
     })
 }) 
+
 
 // CRUD - 'R' Read Post is in response above
 
 
 // CRUD - 'U' Update Post using findOnAndUpdate()
 app.post('/update-item', function(req, res) {
+    console.log("Sending to DB: Update Item")
     db.collection("items").findOneAndUpdate({_id: new mongodb.ObjectID(req.body.id)}, {$set: {text: req.body.text}}, function() {
+        console.log("Update Request - Success: " + req.body.id)
         res.send("Success")
     })
 
@@ -85,7 +87,9 @@ app.post('/update-item', function(req, res) {
 
 // CRUD - 'D' Delete Post using deleteOne()
 app.post('/delete-item', function(req,res) {
+    console.log("Sending to DB: Delete Item")
     db.collection("items").deleteOne({_id: new mongodb.ObjectID(req.body.id)}, function() {
+        console.log("Delete Request - Success: " + req.body.id)
         res.send("Success")
     })
     
@@ -133,4 +137,5 @@ Delete
 
  Questions
  - Why does code fail when we move document.addEventListener() to server.js file ? 
+ > Document DOES NOT work in a node environment, hence you need a seperate file to use document
  */
